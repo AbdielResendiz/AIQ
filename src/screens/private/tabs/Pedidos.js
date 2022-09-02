8455b2c9348815110de6a768b053cf8aa3bf3da9
AldairCruz7 <aldairtusero1@gmail.com>
2022-09-02 12:51:15 -0500

    pedidos

--- src/screens/private/tabs/Pedidos.js
+++ src/screens/private/tabs/Pedidos.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { ScrollView, View, StyleSheet, Text } from 'react-native';
 import { Divider, Image } from 'native-base';
 
+
 const foods = [
   {
     title: "chicken",
@@ -34,10 +35,14 @@ const Pedidos = () => {
     <ScrollView>
       {foods.map((food, index) => (
         <View key={index}>
-          <View style={styles.menuItemStyle}>
-            <Text style={styles.titleStyle}>{food.description}</Text>
-            <Text style={styles.titleStyle}>{food.price}</Text>
-          </View>
+             <View style={styles.cardView}>
+              <View style={{flexDirection:'column'}}> 
+                  <Text style={{textTransform: 'uppercase', color:'#07B83A'}} >Humedad:
+                  {food.description}</Text>  
+                   <Text style={{textTransform: 'uppercase', color:'#07B83A'}} >temperatura:
+                    {props.price}</Text>          
+                         </View>    
+                </View>
           <Divider width={0.5} />
         </View>
       ))}
@@ -55,6 +60,22 @@ const styles = StyleSheet.create({
     fontSize: 19,
     fontWeight: "600",
   },
+  cardView: {
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    shadowColor: "#000",
+    marginHorizontal:8,
+    marginVertical:5,
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  }
+
 });
 
 export default Pedidos
