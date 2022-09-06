736fba66aa42370f077f89bb8e34ce0f7805c64a
Aldair Cruz <aldairtusero1@gmail.com>
2022-09-06 13:42:36 -0500

    avance de anuncions

--- src/screens/private/InicioAds.js
+++ src/screens/private/InicioAds.js
@@ -1,20 +1,149 @@
-import React from 'react';
-import { TouchableOpacity, Text, View} from 'react-native';
+import React,{useState,useCallback} from 'react';
+import { TouchableOpacity,ScrollView,RefreshControl,SafeAreaView} from 'react-native';
 import estilosAIQ from '../../styles/estilosAIQ';
+import {Text, Box, Button, Image, Flex, Center, View} from 'native-base';
+import { alignItems } from 'styled-system';
 
 const InicioAds = (props) => {
+	const [refreshing, setRefreshing] = useState(false);
+  
+  const onRefresh = useCallback(() => {
+		setRefreshing(true);
+		wait(1500).then(() => setRefreshing(false));
+	}, []);
+
+  const arrAnuncios = [
+    {
+      idAd: 1,
+      imagen: '../../../assets/Logos/1.png'
+    },
+    {
+      idAd: 2,
+      imagen: '../../../assets/Logos/1.png'
+    },
+    {
+      idAd: 3,
+      imagen: '../../../assets/Logos/1.png'
+    },
+    {
+      idAd: 4,
+      imagen: '../../../assets/Logos/1.png'
+    },
+  ];
   return (
-    <View style={{
-        flex: 1,
-        flexDirection: 'column',
+    <SafeAreaView flex={5} flexDirection={'column'}>
+    <ScrollView 
+    contentContainerStyle={{paddingBottom:0}}
+    horizontal={true}
+    showsHorizontalScrollIndicator={false}
+    flex={2}>
+    <Box
+     
+      p={3}
+      style={{
+        flexDirection: 'row',
         justifyContent: 'center',
-    }}>
-        <TouchableOpacity 
-            style={estilosAIQ.botonTouch}
-            onPress={() => {props.navigation.navigate('Restaurante')}}>
-            <Text style={estilosAIQ.textBtn}>Ver restaurantes</Text>
-        </TouchableOpacity>
-    </View>
+        flexWrap: 'nowrap',
+        alignContent: 'center',
+        alignItems: 'center'
+        
+      }}>
+      {arrAnuncios.map((item) => {
+        return (
+          <Box key={item.idAd} mb={4}>
+            <TouchableOpacity
+              style={{
+                width: 400 ,
+                height: 250,
+                marginRight: 8, justifyContent: 'center',
+                flexWrap: 'nowrap',
+                alignContent: 'center',
+                alignItems: 'center'
+              }}
+              onPress={() => {console.log(item.idAd)}}>
+              <Image
+                borderRadius={5}
+                source={require('../../../assets/Logos/1.png')}
+                alt='Anuncio'
+                style={{
+                  resizeMode: 'cover',
+                  width: '100%',
+                  height: 200,
+                  
+                }}
+              />
+            </TouchableOpacity>
+          </Box>
+        );
+      })}
+    </Box>
+  
+   
+    </ScrollView>
+
+<View style={{
+  flex: 1,
+  flexDirection: 'column',
+  justifyContent: 'center',
+  flexWrap: 'nowrap',
+  alignContent: 'center',
+  alignItems: 'center'
+
+}}>
+  <TouchableOpacity 
+      style={estilosAIQ.botonTouch}
+      onPress={() => {props.navigation.navigate('Restaurante')}}>
+      <Text style={estilosAIQ.textBtn}>Ver restaurantes</Text>
+  </TouchableOpacity>
+</View>
+<ScrollView 
+    contentContainerStyle={{paddingBottom:0}}
+    horizontal={true}
+    showsHorizontalScrollIndicator={false}
+    flex={2}>
+    <Box
+     
+      p={3}
+      style={{
+        flexDirection: 'row',
+        flexWrap: 'nowrap',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        
+      }}>
+      {arrAnuncios.map((item) => {
+        return (
+          <Box key={item.idAd} mb={4}>
+            <TouchableOpacity
+              style={{
+                width: 400 ,
+                height: 150,
+                marginRight: 8,
+                justifyContent: 'center',
+                flexWrap: 'nowrap', // falta checar
+                alignContent: 'center',
+                alignItems: 'center'
+              }}
+              onPress={() => {console.log(item.idAd)}}>
+              <Image
+                borderRadius={5}
+                source={require('../../../assets/Logos/1.png')}
+                alt='Anuncio'
+                style={{
+                  resizeMode: 'cover',
+                  width: '100%',
+                  height: 200,
+                }}
+              />
+            </TouchableOpacity>
+          </Box>
+        );
+      })}
+    </Box>
+  
+   
+    </ScrollView>
+</SafeAreaView>
   )
 }
 
