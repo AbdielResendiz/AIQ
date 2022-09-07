efe0285e1782bcbd327e2780eca384a194383eeb
isaac-candelario-escobar-lopez <isacel97@gmail.com>
2022-09-07 11:18:12 -0500

    restaurentes funcionales

---
+++ src/api/controlWS.js
@@ -0,0 +1,63 @@
+import axios from "axios";
+
+const source = axios.CancelToken.source();
+const baseUrl = 'https://v-csoft.com/AIQ/MovilR';
+const urlRes = `${baseUrl}/getRestaurantes`;
+const urlAD = `${baseUrl}/getPublicidad`;
+
+export const getRestaurantes = async() => {
+  try{
+    const response = await axios.get(urlRes, {cancelToken: source.token});
+    if (response.status === 200) {
+      return (response.data);
+    } else {
+      throw new Error("Fallo en fetch array restaurantes")
+    }
+  } catch (error) {
+    if(axios.isCancel(error)) {
+      console.log('Data fetching cancelado')
+    } else {
+      console.log('algo pago en funcion getRestaurantes', error)
+    }
+  }
+}
+
+export const getPublicidad = async () => {
+  try{
+    const response = await axios.get(urlAD, {cancelToken: source.token});
+    if (response.status === 200) {
+      return response.data.Publicidad;
+    } else {
+      throw new Error("Fallo en fetch array anuncio")
+    }
+  } catch (error) {
+    if(axios.isCancel(error)) {
+      console.log('Data fetching cancelado')
+    } else {
+      console.log('algo pago en funcion getPublicidad', error)
+    }
+  }
+}
+
+export const getMenu = async (idRes) => {
+  const idRest = idRes;
+  const urlMenu = `${baseUrl}/getPublicidad$`
+  try{
+    setCargando(true);
+    const response = await axios.get(url, {cancelToken: source.token});
+    if (response.status === 200) {
+      setArrAlimentos(response.data);
+      setCargando(false);
+      return;
+    } else {
+      throw new Error("Fallo en fetch array restaurantes")
+    }
+  } catch (error) {
+    if(axios.isCancel(error)) {
+      console.log('Data fetching cancelado')
+    } else {
+      setErrorFlag(true);
+      setCargando(false);
+    }
+  }
+}
